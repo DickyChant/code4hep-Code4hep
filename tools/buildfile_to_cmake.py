9273b366e88fcbf0500ba2b6b750d19ca60375f1
Kevin Pedro <kpedro88@gmail.com>
2026-06-17 18:56:19 -0500

    converted BuildFile.xml into CMakeLists.txt

--- tools/buildfile_to_cmake.py
+++ tools/buildfile_to_cmake.py
@@ -102,6 +102,10 @@ SCRAM_TO_CMAKE_TARGETS: dict[str, str] = {
     "catch2":    "Catch2::Catch2",
     "dd4hep-core": "DD4hep::Core",
     "dd4hep":    "DD4hep::DDAlign DD4hep::DDCond",
+    "geant4core": "${Geant4_LIBRARIES}",
+    "xerces-c": "XercesC::XercesC",
+    "hepmc3": "HepMC3::HepMC3",
+    "pythia8": "Pythia8::Pythia8",
 }
 
 # ---------------------------------------------------------------------------
