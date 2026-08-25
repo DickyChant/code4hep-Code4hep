10bd34cbef155a847b5b7017a69db76f8f70376f
Kevin Pedro <kpedro88@gmail.com>
2026-08-25 15:57:14 -0500

    temporary FileInPath fix

--- Code4hep/Geometry/test/runTest.sh
+++ Code4hep/Geometry/test/runTest.sh
@@ -6,5 +6,8 @@ export DD4HEP_XML_DIR=$(scram_tag dd4hep-core DD4HEP_CORE_BASE)/DDDetectors/comp
 ln -sf ${DD4HEP_XML_DIR}/elements.xml .
 ln -sf ${DD4HEP_XML_DIR}/materials.xml .
 C4H_DIR=$(readlink -f ${PWD}/../../../)
-export CMSSW_SEARCH_PATH=$CMSSW_SEARCH_PATH:${C4H_DIR}
+INSTALL_DIR=$(readlink -f ${C4H_DIR}/../)
+# this is a very temporary hack around edm::FileInPath restrictions
+export CMSSW_SEARCH_PATH=${C4H_DIR}
+export CMSSW_BASE=${INSTALL_DIR}
 cmsRun testGeo.py
